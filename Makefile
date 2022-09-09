2ec4e6bda0eebbf86b4af9562f51e0054a034b46
Ashwin Aralamallige Gopalakrishna <ashwin100196@gmail.com>
2022-09-10 03:25:45 +0530

    included cifar10 and docker

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-
+build:
+	@echo Building docker image
+	docker build --tag lightninghydra:latest .
 help:  ## Show help
 	@grep -E '^[.a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
---
+++ src/datamodules/cifar10_datamodule.py
@@ -0,0 +1,66 @@
+from typing import Any, Dict, Optional, Tuple
+
+import torch
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
+from torchvision.datasets import MNIST, CIFAR10
+from torchvision.transforms import transforms
+
+BATCH_SIZE = 256 if torch.cuda.is_available() else 64
+
+class CIFAR10DataModule(LightningDataModule):
+    def __init__(
+            self,
+            data_dir: str = "data/",
+            train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
+            batch_size: int = 64,
+            num_workers: int = 0,
+            pin_memory: bool = False
+        ):
+        super().__init__()
+        self.data_dir = data_dir
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+        self.dims = (3, 32, 32)
+        self.num_classes = 10
+
+    def prepare_data(self):
+        # download
+        CIFAR10(self.data_dir, train=True, download=True)
+        CIFAR10(self.data_dir, train=False, download=True)
+
+    def setup(self, stage=None):
+
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit" or stage is None:
+            cifar_full = CIFAR10(self.data_dir, train=True, transform=self.transform)
+            self.cifar_train, self.cifar_val = random_split(cifar_full, [45000, 5000])
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test" or stage is None:
+            self.cifar_test = CIFAR10(self.data_dir, train=False, transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.cifar_train, batch_size=BATCH_SIZE)
+
+    def val_dataloader(self):
+        return DataLoader(self.cifar_val, batch_size=BATCH_SIZE)
+
+    def test_dataloader(self):
+        return DataLoader(self.cifar_test, batch_size=BATCH_SIZE)
+
+
+if __name__ == "__main__":
+    import hydra
+    import omegaconf
+    import pyrootutils
+
+    root = pyrootutils.setup_root(__file__, pythonpath=True)
+    cfg = omegaconf.OmegaConf.load(root / "configs" / "datamodule" / "cifar10.yaml")
+    cfg.data_dir = str(root / "data")
+    _ = hydra.utils.instantiate(cfg)
